65cb2e0d9419d61126ed5e6df26f9af8a446b72f
Tousif Islam <tousifislam099@gmail.com>
2025-04-18 02:05:23 +0530

    Update Soil.cpp

--- Soil.cpp
+++ Soil.cpp
@@ -1,10 +1,19 @@
-// Pin setup
 const int soilPin = A0;
-const int ledPin = 13;
-const int dryness = 600;
+const int MP = 13;
+const int dryThreshold = 500;
+const int LP = 9;
+const int trig = 8;
+const int echo = 7;
+const int DD = 5;
+
 void setup() {
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
+  pinMode(LP, OUTPUT);
+  pinMode(echo, INPUT);
+  pinMode(trig, OUTPUT);
+  pinMode(MP, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(soilPin, INPUT);
 }
 
 void loop() {
@@ -12,14 +21,41 @@ void loop() {
   Serial.print("Soil Reading: ");
   Serial.println(moisture);
 
-  if (moisture > dryness) {
+  checkDistance();
+
+  if (moisture > dryThreshold) {
     Serial.println("Soil is dry.");
-    digitalWrite(ledPin, HIGH);
-    // You could turn on a motor or buzzer here
-  } else {
+    digitalWrite(LED_BUILTIN, HIGH);
+  } 
+  else {
     Serial.println("Soil is moist.");
-    digitalWrite(ledPin, LOW);
+    digitalWrite(LED_BUILTIN, LOW);
   }
 
-  delay(2000);
+  delay(100); 
+}
+
+
+void checkDistance() {
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+
+  long duration = pulseIn(echo, HIGH); 
+  int distance = duration * 0.0342 / 2;
+
+  if (distance <= DD) {
+    Serial.print("Water is enough: ");
+    Serial.print(distance);
+    Serial.println(" cm");             
+    digitalWrite(LP, LOW);
+  } 
+  else if (distance > 20) {
+    Serial.println("Water is less... Fill it up!");
+    Serial.print(distance);
+    Serial.println(" cm");
+    digitalWrite(LP, HIGH);
+  }
 }
